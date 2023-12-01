5c5015c90bcecdff43b6459d79951fea15882dc1
Bruin <sander.debruin@nslogin.nl>
2023-12-01 18:48:15 +0100

    add 2023:

---
+++ 2023/1/part_2/solve.py
@@ -0,0 +1,46 @@
+import os
+
+def get_numbers_from_string(line: str):
+    number_strings = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    ]
+
+    for number_s in number_strings:
+        line.find(number_s)
+
+
+def solve(calibration_document: str) -> None:
+    """
+    Print the sum of all of the calibration values
+    """
+    
+
+    total = 0
+
+    for s in calibration_document:
+        # Get al numbers from string
+        numbers = "".join(filter(lambda s: s.isnumeric(), s))
+        # Get first and last digit only
+        numbers = int(numbers[0] + numbers[-1])
+        # Add to sum
+        total += numbers
+        
+    print(total)
+
+if __name__ == "__main__":
+    input_file = "input.txt"
+
+    path = os.path.join(os.path.abspath(__file__), "..", input_file)
+    with open(path, "r", encoding="utf-8") as f:
+        inp = f.readlines()
+
+    solve(inp)
