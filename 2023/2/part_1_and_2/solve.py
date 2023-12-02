fa512e25bd7789c2789a8305ae351b9aa078aa5d
Niels IJpelaar <niels.ijpelaar@ns.nl>
2023-12-02 21:21:01 +0100

    Solved part two

--- 2023/2/part_1_and_2/solve.py
+++ 2023/2/part_1_and_2/solve.py
@@ -7,9 +7,9 @@ import re
 @dataclass
 class Game:
     id: int
-    blues: int
-    reds: int
-    greens: int
+    blue: int
+    red: int
+    green: int
 
     @staticmethod
     def from_string(line: str) -> Game:
@@ -20,15 +20,19 @@ class Game:
         blue_idxs = [r.start() for r in re.finditer('blue', line)]
         green_idxs = [r.start() for r in re.finditer('green', line)]
 
-        reds = [int(line[0: red_idx - 1].split(" ")[-1]) for red_idx in red_idxs]
-        blues = [int(line[0: blue_idx - 1].split(" ")[-1]) for blue_idx in blue_idxs]
-        greens = [int(line[0: green_idx - 1].split(" ")[-1]) for green_idx in green_idxs]
+        red = max([int(line[0: red_idx - 1].split(" ")[-1]) for red_idx in red_idxs])
+        blue = max([int(line[0: blue_idx - 1].split(" ")[-1]) for blue_idx in blue_idxs])
+        green = max([int(line[0: green_idx - 1].split(" ")[-1]) for green_idx in green_idxs])
 
-        return Game(id, blues, reds, greens)
+        return Game(id, blue, red, green)
     
     @property
     def is_possible(self):
-        return max(self.reds) <= 12 and max(self.greens) <= 13 and max(self.blues) <= 14
+        return self.red <= 12 and self.green <= 13 and self.blue <= 14
+    
+    @property
+    def power(self):
+        return self.red * self.green * self.blue
 
 
 def solve(games: str) -> None:
@@ -36,11 +40,15 @@ def solve(games: str) -> None:
     Print the sum of all of the calibration values
     """
 
-    games = map(Game.from_string, games)
-    games = filter(lambda g: g.is_possible, games)
+    games = list(map(Game.from_string, games))
 
-    output = sum(map(lambda g: g.id, games))
-    print(output)
+    power = sum(map(lambda g: g.power, games))
+    
+    possible_games = filter(lambda g: g.is_possible, games)
+    output = sum(map(lambda g: g.id, possible_games))
+    
+    print("Assignment 1:", output)
+    print("Assignment 2:", power)
 
 
 if __name__ == "__main__":
