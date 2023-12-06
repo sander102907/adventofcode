e7f297eb85d9d1f99293a0ef7dbefa069674734a
Bruin <sander.debruin@nslogin.nl>
2023-12-06 09:04:57 +0100

    set up day 6 part 1

---
+++ 2023/6/part_2/solve.py
@@ -0,0 +1,13 @@
+import os
+
+def solve(schematic: str) -> None:
+    pass
+
+if __name__ == "__main__":
+    input_file = "input.txt"
+
+    path = os.path.join(os.path.abspath(__file__), "..", "..", "input", input_file)
+    with open(path, "r", encoding="utf-8") as f:
+        inp = f.readlines()
+
+    solve(inp)
