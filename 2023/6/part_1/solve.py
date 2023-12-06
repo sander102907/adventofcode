c448d58761df1fdf9c6bcc2e330fe754fabdbb56
Bruin <sander.debruin@nslogin.nl>
2023-12-06 09:58:31 +0100

    solve day 6

--- 2023/6/part_1/solve.py
+++ 2023/6/part_1/solve.py
@@ -1,7 +1,33 @@
 import os
+import math
+import re
 
-def solve(schematic: str) -> None:
-    pass
+def compute_amt_ways(total_t: int, distance_to_beat: int) -> int:
+    dis = total_t**2 - 4 * distance_to_beat
+    sqrt_val = math.sqrt(abs(dis))
+
+    button_min_time = (-total_t + sqrt_val)/(2)
+    button_max_time = (-total_t - sqrt_val)/(2)
+
+    button_min_time = math.floor(abs(button_min_time) + 1)
+    button_max_time = math.ceil(abs(button_max_time) - 1)
+
+    return button_max_time - button_min_time + 1
+
+
+
+def solve(lines: str) -> None:
+    times = re.findall(r"\d+", lines[0])
+    distances = re.findall(r"\d+", lines[1])
+
+    output = 1
+
+    for time, distance in zip(times, distances):
+        output *= compute_amt_ways(int(time), int(distance))
+
+    print(output)
+
+
 
 if __name__ == "__main__":
     input_file = "input.txt"
--- 2023/6/part_2/solve.py
+++ 2023/6/part_2/solve.py
@@ -1,7 +1,26 @@
 import os
+import math
 
-def solve(schematic: str) -> None:
-    pass
+def compute_amt_ways(total_t: int, distance_to_beat: int) -> int:
+    dis = total_t**2 - 4 * distance_to_beat
+    sqrt_val = math.sqrt(abs(dis))
+
+    button_min_time = (-total_t + sqrt_val)/(2)
+    button_max_time = (-total_t - sqrt_val)/(2)
+
+    button_min_time = math.floor(abs(button_min_time) + 1)
+    button_max_time = math.ceil(abs(button_max_time) - 1)
+
+    return button_max_time - button_min_time + 1
+
+
+
+def solve(lines: str) -> None:
+    output = compute_amt_ways(51926890, 222203111261225)
+
+    print(output)
+
+
 
 if __name__ == "__main__":
     input_file = "input.txt"
