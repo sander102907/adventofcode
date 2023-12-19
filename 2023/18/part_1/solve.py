e779c9238a85a9f8de34bc1d302a65d36572be4b
Bruin <sander.debruin@nslogin.nl>
2023-12-19 10:22:48 +0100

    solve day 18

--- 2023/18/part_1/solve.py
+++ 2023/18/part_1/solve.py
@@ -1,7 +1,45 @@
 import os
+from dataclasses import dataclass
+from typing import List
+from shapely import Point, Polygon
+
+@dataclass
+class Instruction:
+    direction: str
+    meters: int
+    colour: str
+
+    def __post_init__(self):
+        self.meters = int(self.meters)
+
+def dig(dig_plan: List[Instruction]):
+    start = Point(0, 0)
+
+    points = [start]
+    m3 = 0
+
+    for instruction in dig_plan:
+        m3 += instruction.meters
+        if instruction.direction == "U":
+            new_point = Point(points[-1].x, points[-1].y - instruction.meters)
+        if instruction.direction == "R":
+            new_point = Point(points[-1].x + instruction.meters, points[-1].y)
+        if instruction.direction == "L":
+            new_point = Point(points[-1].x - instruction.meters, points[-1].y)
+        if instruction.direction == "D":
+            new_point = Point(points[-1].x, points[-1].y + instruction.meters)
+
+        points.append(new_point)
+
+    return Polygon(points), m3
+
 
 def solve(lines: str) -> None:
-    pass
+    dig_plan = [Instruction(*line.strip().split(" ")) for line in lines]
+
+    polygon, m3 = dig(dig_plan)
+
+    print(m3 + polygon.area - m3 / 2 + 1)
 
 if __name__ == "__main__":
     input_file = "input.txt"
--- 2023/18/part_2/solve.py
+++ 2023/18/part_2/solve.py
@@ -1,7 +1,53 @@
 import os
+from dataclasses import dataclass
+from typing import List
+from shapely import Point, Polygon
+
+@dataclass
+class Instruction:
+    direction: int
+    meters: int
+
+    @staticmethod
+    def from_string(line: str):
+        instr = line.strip().split(" ")[-1]
+
+        meters = int(instr[2:7], 16)
+        direction = int(instr[7], 16)
+
+        return Instruction(direction, meters)
+
+    def __post_init__(self):
+        self.meters = int(self.meters)
+
+def dig(dig_plan: List[Instruction]):
+    start = Point(0, 0)
+
+    points = [start]
+    m3 = 0
+
+    for instruction in dig_plan:
+        m3 += instruction.meters
+        if instruction.direction == 3:
+            new_point = Point(points[-1].x, points[-1].y - instruction.meters)
+        if instruction.direction == 0:
+            new_point = Point(points[-1].x + instruction.meters, points[-1].y)
+        if instruction.direction == 2:
+            new_point = Point(points[-1].x - instruction.meters, points[-1].y)
+        if instruction.direction == 1:
+            new_point = Point(points[-1].x, points[-1].y + instruction.meters)
+
+        points.append(new_point)
+
+    return Polygon(points), m3
+
 
 def solve(lines: str) -> None:
-    pass
+    dig_plan = [Instruction.from_string(line) for line in lines]
+
+    polygon, m3 = dig(dig_plan)
+
+    print(m3 + polygon.area - m3 / 2 + 1)
 
 if __name__ == "__main__":
     input_file = "input.txt"
